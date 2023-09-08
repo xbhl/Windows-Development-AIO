31c6e3a31fccaaefac9ac5c802595ebe315350a4
Abstract <vokunocode@protonmail.com>
2023-09-08 14:32:55 +0100

    Proof of project development
    Early stages, does not work

---
+++ API/FileManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.IO;
+
+namespace Windows_Development_AIO.API
+{
+    public class FileManager
+    {
+        private bool checkFile(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+
+        public string Download(string url, string filePath)
+        {
+            if (checkFile(filePath))
+            {
+                return filePath;
+            }
+            else
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    var response = client.GetAsync(url).Result;
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var fileBytes = response.Content.ReadAsByteArrayAsync().Result;
+                        File.WriteAllBytes(filePath, fileBytes);
+                        return filePath;
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Failed to download the file.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ API/Sources.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Windows_Development_AIO.API
+{
+    public class CPP
+    {
+        public enum Files
+        {
+            vcpkg,
+            mingw,
+        }
+
+        public static string GetSource(Files file)
+        {
+            switch (file)
+            {
+                case Files.vcpkg:
+                    return "https://netcologne.dl.sourceforge.net/project/mingw/Installer/mingw-get-setup.exe";
+                case Files.mingw:
+                    return "https://github.com/microsoft/vcpkg";
+                default:
+                    throw new ArgumentException("Missing file args");
+            }
+        }
+    }
+}
